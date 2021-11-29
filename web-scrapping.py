6b5958ad5b47ca3654da6bdc90f990bbd0753613
Duckies Mac <javi@Duckiess-iMac.local>
2021-11-28 19:31:08 -0800

    Notes: add comments clean up variable names

--- web-scrapping.py
+++ web-scrapping.py
@@ -4,24 +4,23 @@ import json
 import requests
 
 
+# domain
+facts = 'https://www.factretriever.com'
 
-rinos = 'https://www.factretriever.com'
-musk = 'https://en.wikipedia.org/wiki/Elon_Musk'
+#genrates request to domain stores response to result variable
+result = requests.get(facts)
 
+#empty dictionary to store results
 results = {}
 
-result = requests.get(rinos)
+#Beautuful soup parses the html of the response and stores as text into our soup variable
 soup = BeautifulSoup(result.text, "html.parser")
+
+#loop over soup searching for links and text, storing our formatted results into our dictionary
 for i, li in enumerate(soup.find_all('a', href=True)):
         results[i] = {"Fact":  li.text.strip("\n\n"), "Link": 'https://www.factretriever.com' + li['href'] }
 
-# headers = [header for header in doc.find_all("a", href=True)]
-# links = [header.li for header in doc.find("li", {"class": "toclevel-1"})]
-
-# for content in tesla:
-#     tesla_results[content.li] = content.a
-# result_json = json.dumps(results)
+#create a file called facts.json and writes our results into our json file.
 with open('facts.json', 'w') as json_file:
     json.dump(results, json_file)
 
-print(results)
