56fb3c653dd3acdad03a748b2db46f9865ea78dc
Duckies Mac <javi@Duckiess-iMac.local>
2021-11-28 21:33:33 -0800

    update add filter for empty strings

--- web-scrapping.py
+++ web-scrapping.py
@@ -17,19 +17,25 @@ soup = BeautifulSoup(result.text, "html.parser")
 
 #loop over soup searching for links and text, storing our formatted results into our dictionary
 for i, li in enumerate(soup.find_all('a', href=True)):
-        results[li.text.strip("\n\n")] = {"Link": 'https://www.factretriever.com' + li['href'] }
+        if li.text != "":
+            results[li.text.strip("\n\n")] = {"Link": 'https://www.factretriever.com' + li['href'] }
 
 #create a file called facts.json and writes our results into our json file.
 with open('facts.json', 'w') as json_file:
     json.dump(results, json_file)
+
 # open json facts file
 json_file = open('facts.json')
+
 # load data in json format and make searchable
 data = json.load(json_file)
+
 #capture terminal input
 search = str(input())
+
 #print data if it exist
 if data[search]: 
     print(data[search])
+
 #closes file after completed search
 json_file.close()
